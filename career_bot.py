98de63de1be47ec4ea45e5e6583524aebad95fda
ChandanaKodari04 <160821077+ChandanaKodari04@users.noreply.github.com>
2025-03-02 16:25:44 +0530

    Update career_bot.py

--- career_bot.py
+++ career_bot.py
@@ -83,7 +83,7 @@ st.markdown(
         opacity: 1;
     }}
     </style>
-    <div class="background-text">"Your Future is Created by What You Do Today,Build Your Career Now""</div>
+    <div class="background-text">"The Best Way to Predict Your Future is to Create It..."</div>
     """,
     unsafe_allow_html=True
 )
