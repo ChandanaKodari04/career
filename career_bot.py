eec27a30f3a797713e20418ae0ae61f40547efe2
ChandanaKodari04 <160821077+ChandanaKodari04@users.noreply.github.com>
2025-03-02 13:27:47 +0530

    Update career_bot.py

--- career_bot.py
+++ career_bot.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import google.generativeai as genai  # Google Gemini API
 import pandas as pd  # Pandas for career Q&A data handling
-
+import os
 
 
 # ✅ Configure Gemini API
