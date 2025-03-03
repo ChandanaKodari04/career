39a1fef1a576ec3d34e6f34185ee54adde55f01d
ChandanaKodari04 <160821077+ChandanaKodari04@users.noreply.github.com>
2025-03-03 10:07:40 +0530

    Update career_bot.py

--- career_bot.py
+++ career_bot.py
@@ -71,12 +71,8 @@ st.markdown(
         left: -55%;
         font-size: 4rem;
         font-weight: bold;
-        color: rgba(0, 0.5, 5, 0.2); /* Transparent effect */
-        z-index: -1;
         text-align: left;
         width: 40%;
-       text-shadow: 2px 2px 2px rgba(10, 10, 0, 0.8), 2px 2px 2px rgba(0, 0, 0, 0.8);
-       color:none;
     }}
     h1 {{
         position: relative;
